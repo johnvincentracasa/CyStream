decc0f03620635ad8bd89c78aeb1fbf6f6e9e5ed
John Vincent Racasa <johnvincentracasa@gmail.com>
2021-02-27 22:39:53 +0800

    Add initial streamEdit functionality and navigation

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
       <Router history={history}>
         <Header />
         <Route path="/" exact component={StreamList} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/edit/:id" exact component={StreamEdit} />
         <Route path="/streams/new" exact component={StreamCreate} />
         <Route path="/streams/delete" exact component={StreamDelete} />
         <Route path="/streams/show" exact component={StreamShow} />
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function StreamEdit() {
-  return <div>StreamEdit</div>;
+function StreamEdit({ stream }) {
+  const renderEdit = (stream) => {
+    if (stream) {
+      return (
+        <div className="item">
+          <div className="content">
+            <div className="header">{stream.title}</div>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return <div className="ui items celled list">{renderEdit(stream)}</div>;
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
+};
+
+export default connect(mapStateToProps)(StreamEdit);
